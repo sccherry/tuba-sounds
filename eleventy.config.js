999f5fafd87af95c2af08f0d6e19a956b7333eb8
Steve Cherry <stevenccherry@gmail.com>
2021-02-09 15:55:15 +0000

    Add Netlify CMS

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,8 @@ const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
+  eleventyConfig.addPassthroughCopy('src/admin/*.yml');
+
   return {
     dataTemplateEngine: 'njk',
     dir: {
